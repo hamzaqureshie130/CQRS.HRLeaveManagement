e19e2c6ed67d2a183cea864b784c30d496f420fc
mudassar96 <mudassarmehdi96@gmail.com>
2025-11-17 22:20:50 +0500

    update

---
+++ HRLeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+          CreateMap<Domain.LeaveType, DTOs.LeaveTypeDto>().ReverseMap();
+          CreateMap<Domain.LeaveAllocation, DTOs.LeaveAllocationDto>().ReverseMap();
+          CreateMap<Domain.LeaveRequest, DTOs.LeaveRequestDto>().ReverseMap();
+        }
+    }
+}
