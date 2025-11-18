b72ed9f2c96b1c5085f90079eb080667ad5aad8f
mudassar96 <mudassarmehdi96@gmail.com>
2025-11-18 10:28:01 +0500

    update

---
+++ HRLeaveManagement.Application/Features/LeaveAllocation/Requests/Commands/CreateLeaveAllocationCommand.cs
@@ -0,0 +1,15 @@
+﻿using HRLeaveManagement.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveAllocation.Requests.Commands
+{
+    public class CreateLeaveAllocationCommand : IRequest<int>
+    {
+        public LeaveAllocationDto LeaveAllocationDto { get; set; }
+    }
+}
